0b5bacb4fd008e736eb076d9ebc6099cc08c54f7
Hansi <hansisgithub@gmail.com>
2016-06-21 10:53:26 +0200

    initial

---
+++ wpftest/viewmodel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace wpftest.viewmodel
+{
+	class MainViewModel
+	{
+		public MainViewModel()
+		{
+			Messenger.Default.Register<GenericMessage<bool>>(this, HandleChecked);
+		}
+
+		private void HandleChecked(GenericMessage<bool> msg)
+		{
+			var value = msg.Content;
+			Console.WriteLine("vom MainViewModel: " + value);
+		}
+
+		public string Name { get; set; }
+	}
+}
---
+++ wpftest/viewmodel/coyoteviewmodel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace wpftest.viewmodel
+{
+	class coyoteviewmodel
+	{
+
+		public bool Checked
+		{
+			
+			set
+			{
+				Console.WriteLine(value);
+				Messenger.Default.Send( new GenericMessage<bool>(value));
+				Messenger.Default.Send(new BuzzeMessage());
+			} 
+		}
+	}
+
+	class BuzzeMessage{} 
+}
